816b21a0707e26440e931704b4f928a99dd10a21
Gao Tianxing <gtx_122@126.com>
2015-05-31 19:06:39 +0800

    再原有模板上改进
    1.增加了分类的功能
    2.增加了搜索
    3.增加了文章翻页

--- template/scripts.js
+++
@@ -1,4 +0,0 @@
-monName = new Array("JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC");
-now = new Date;
-dtTime=monName[now.getMonth()] + "," +now.getDay()+" "+ now.getFullYear();
-document.getElementById("dtTime").innerHTML=dtTime;
